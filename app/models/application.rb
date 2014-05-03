e432bc094cc2e0ede7b071c0aad6761bd1a93b29
Kim Daekwon <propellerheaven@gmail.com>
2014-05-04 01:02:18 +0900

    Define model relation

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,2 +1,3 @@
 class Application < ActiveRecord::Base
+  has_many :docker_servers, through: :applications_docker_servers
 end
--- app/models/docker_server.rb
+++ app/models/docker_server.rb
@@ -1,2 +1,3 @@
 class DockerServer < ActiveRecord::Base
+  has_many :applications, through: :applications_docker_servers
 end
