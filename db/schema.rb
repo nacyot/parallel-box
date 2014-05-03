67649765563f86400408ec5c3b8deb06775defa1
Kim Daekwon <propellerheaven@gmail.com>
2014-05-04 00:42:17 +0900

    Delete duplicatied migration file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140503105636) do
+ActiveRecord::Schema.define(version: 20140503105345) do
 
   create_table "applications", force: true do |t|
     t.string   "name",       null: false
