7bd506adcb61773c2130bdb311ee396d717fdb1b
Kim Daekwon <propellerheaven@gmail.com>
2014-05-04 01:01:54 +0900

    Add some_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root 'main#index'
+
+  resources :docker_servers
+  resources :docker_registries
+  resources :applications
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
