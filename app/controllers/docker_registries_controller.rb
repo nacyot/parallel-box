228211cca083fc66bcc4db0bcd0a3d15b8d6513c
Kim Daekwon <propellerheaven@gmail.com>
2014-05-04 14:14:52 +0900

    Modify some views

--- app/controllers/docker_registries_controller.rb
+++ app/controllers/docker_registries_controller.rb
@@ -1,11 +1,14 @@
 class DockerRegistriesController < ApplicationController
   def index
+    @registries = DockerRegistry.all
   end
 
   def show
+    @registry = DockerRegistry.find(params[:id])
   end
   
   def new
+    @registry = DockerRegistry.new
   end
 
   def create
