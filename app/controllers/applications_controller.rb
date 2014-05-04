39489638b5e96673c0755f3acc1e45a217a8f6e9
Kim Daekwon <propellerheaven@gmail.com>
2014-05-04 12:29:41 +0900

    Add basic reousce methods to controllers

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,22 +1,39 @@
 class ApplicationsController < ApplicationController
   def index
+    @apps = Application.all
   end
 
   def show
+    @app = Application.find(params[:id])
+    @stage = @app.docker_servers.select{|server| server.server_type == "stage"}
+    @deploy = @app.docker_servers.select{|server| server.server_type == "deploy"}
+    @tags = @app.docker_registry.box(@app).tags
   end
   
   def new
+    @app = Application.new
   end
 
   def create
+    if Application.create(application_params)
+      redirect_to applications_url
+    else
+      redirect_to :back
+    end
   end
 
   def edit
+    @app = Application.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+  def application_params
+    params.require(:application).permit(:name, :url)
   end
 end
