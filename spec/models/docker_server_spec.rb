8af39631ab1fb19402201827c97149e9ae797400
Kim Daekwon <propellerheaven@gmail.com>
2014-05-05 14:31:44 +0900

    Update specs

--- spec/models/docker_server_spec.rb
+++ spec/models/docker_server_spec.rb
@@ -2,14 +2,15 @@
 #
 # Table name: docker_servers
 #
-#  id          :integer          not null, primary key
-#  name        :string(255)
-#  host        :string(255)
-#  port        :string(255)
-#  type        :string(255)
-#  description :string(255)
-#  created_at  :datetime
-#  updated_at  :datetime
+#  id             :integer          not null, primary key
+#  name           :string(255)
+#  host           :string(255)
+#  port           :string(255)
+#  server_type    :string(255)
+#  description    :string(255)
+#  application_id :integer
+#  created_at     :datetime
+#  updated_at     :datetime
 #
 
 require 'spec_helper'
